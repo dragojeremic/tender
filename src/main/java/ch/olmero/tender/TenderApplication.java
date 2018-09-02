1fe53988cc4b48f90b9c5ef4906ba9098fd3c9b3
Drago Jeremic <drago.jeremic@prodyna.com>
2018-09-02 19:47:46 +0200

    Initial commit

---
+++ src/main/java/ch/olmero/tender/TenderApplication.java
@@ -0,0 +1,12 @@
+package ch.olmero.tender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenderApplication.class, args);
+	}
+}
